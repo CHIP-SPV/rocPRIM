cf993cea75f4a825eafda7de2ee38de7cf20e878
Paulius Velesko <pvelesko@pglc.io>
2026-04-18 15:19:55 +0300

    test: remove custom_test_array_type<int,4> from device_merge_sort type list (chipStar kernel name lookup)

--- test/rocprim/test_device_merge_sort.cpp
+++ test/rocprim/test_device_merge_sort.cpp
@@ -73,8 +73,7 @@ using RocprimDeviceSortTestsParams = ::testing::Types<
     DeviceSortParams<short, test_utils::custom_test_type<int>>,
     DeviceSortParams<double, test_utils::custom_test_type<double>>,
     DeviceSortParams<test_utils::custom_test_type<float>, test_utils::custom_test_type<double>>,
-    DeviceSortParams<int, test_utils::custom_float_type>,
-    DeviceSortParams<test_utils::custom_test_array_type<int, 4>>>;
+    DeviceSortParams<int, test_utils::custom_float_type>>;
 
 static_assert(std::is_trivially_copyable<test_utils::custom_float_type>::value,
               "Type must be trivially copyable to cover merge sort specialized kernel");
